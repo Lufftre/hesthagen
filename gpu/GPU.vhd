beb922af1b47d8855a52e2f8a7ad2b230026a215
ludvignoring <ludvig.noring@gmail.com>
2015-05-10 21:48:45 +0200

    2 hästar

--- gpu/GPU.vhd
+++ gpu/GPU.vhd
@@ -133,8 +133,9 @@ begin
   process(CLK) begin
     if rising_edge(CLK) then
       if xctr > xpos1 and xctr < xpos1 + 5 and yctr > ypos1 and yctr < ypos1 + 5 then
-        pixel_to_vga <= "11111111";
+        pixel_to_vga <= "00000011";
       elsif xctr > xpos2 and xctr < xpos2 + 5 and yctr > ypos2 and yctr < ypos2 + 5 then
+        pixel_to_vga <= "11100000";
       else
         pixel_to_vga <= pixel_color;
       end if;
