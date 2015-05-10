49fb11033265d251804132f69766f111f79977bf
ludvignoring <ludvig.noring@gmail.com>
2015-05-10 16:21:06 +0200

    sublime commit

--- cpu/CPU.vhd
+++ cpu/CPU.vhd
@@ -2,7 +2,7 @@ library IEEE;
     use IEEE.STD_LOGIC_1164.ALL;
     use IEEE.NUMERIC_STD.ALL;
     use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
+-- test 123
 entity CPU is
     Port ( 
         CLK : in  STD_LOGIC;
