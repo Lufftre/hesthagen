651dd46af8f4d53c94f032197af61855d8c9b12c
ludvignoring <ludvig.noring@gmail.com>
2015-05-11 00:27:36 +0200

    häst-sprite

--- gpu/hest.vhd
+++ gpu/hest.vhd
@@ -13,7 +13,7 @@ entity hest is
 end entity;
 
 architecture rtl of hest is 
-  signal pixel_counter : integer range 0 to 255;
+  signal pixel_counter : integer range 0 to 127;
   signal rad : std_logic_vector(5 downto 0);
   type hest_type is array (0 to 255) of std_logic_vector(2 downto 0);
   signal hest : hest_type := (
