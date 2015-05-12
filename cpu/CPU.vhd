21a4655d9f275e7243d4ef242e6d196524250cac
ludvignoring <ludvig.noring@gmail.com>
2015-05-12 22:35:31 +0200

    360noscope

--- cpu/CPU.vhd
+++ cpu/CPU.vhd
@@ -144,10 +144,10 @@ architecture rtl of CPU is
     signal lastvalue : std_logic := '0';
 
 
-    signal delta : sfixed(0 downto -10) := to_sfixed(0, 0, -10);
+    signal delta : sfixed(0 downto -9) := to_sfixed(0, 0, -9);
     signal xpos_real : sfixed(9 downto -4) := to_sfixed(320, 9, -4);
     signal ypos_real : sfixed(9 downto -4) := to_sfixed(320, 9, -4);
-    signal jstk_x : integer range -512 to 511;
+    signal jstk_x : std_logic_vector(9 downto 0);
     --signal delta_x : sfixed(9 downto )
     
 begin
@@ -158,9 +158,10 @@ begin
     process(NEW_FRAME) begin
         if rising_edge(NEW_FRAME) then
 
-            jstk_x <= to_integer(signed((joystick1(25 downto 24) & joystick1(39 downto 32)) xor "1000000000"));
+            jstk_x <= (joystick1(25 downto 24) & joystick1(39 downto 32)) xor "1000000000";
 
-            delta <= resize(4,0,-10);
+
+            delta <= to_sfixed(jstk_x,0,-9);
 
             xpos_real <= resize(xpos_real + delta,9,-4);
             xpos_int <= to_integer(xpos_real);
